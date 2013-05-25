37737d340303050166a252b5ca96cea5d3ba06e9
khalahan <khal@dot-bit.org>
2013-05-25 17:15:49 +0200

    Update checkpoints with namecoin hashs (mainnet & testnet) Checkpoints after block 11111 will be enabled when merged mining will be activated

--- src/checkpoints.cpp
+++ src/checkpoints.cpp
@@ -35,34 +35,38 @@ namespace Checkpoints
     // + Contains no strange transactions
     static MapCheckpoints mapCheckpoints =
         boost::assign::map_list_of
-        ( 11111, uint256("0x0000000069e244f73d78e8fd29ba2fd2ed618bd6fa2ee92559f542fdb26e7c1d"))
-        ( 33333, uint256("0x000000002dd5588a74784eaa7ab0507a18ad16a236e7b1ce69f00d7ddfb5d0a6"))
-        ( 74000, uint256("0x0000000000573993a3c9e41ce34471c079dcf5f52a0e824a81e7f953b8661a20"))
-        (105000, uint256("0x00000000000291ce28027faea320c8d2b054b2e0fe44a773f3eefb151d6bdc97"))
+        ( 11111, uint256("0x000000000001dea5779d3df5960afe9f5a40c0c38b1ff6a0b896deaa9712ee50"))
+		/* Namecoin checkpoints
+        ( 33333, uint256("0xeb01e9beca9e791cdc8cef5669308080e7692fc1f884a44c6ca982b1abb350d6"))
+        ( 74000, uint256("0x4c5c8c3acbedac395616096d679e31b4c4c93725f91de1c9d9fd2f00c72df2ed"))
+        (105000, uint256("0x09bc8d10f7d8f808b8cf98bd87d6b0333213cd7a812024623d240f0dd15afac2"))
+		*/
+		/* Bitcoin checkpoints
         (134444, uint256("0x00000000000005b12ffd4cd315cd34ffd4a594f430ac814c91184a0d42d2b0fe"))
         (168000, uint256("0x000000000000099e61ea72015e79632f216fe6cb33d7899acb35b75c8303b763"))
         (193000, uint256("0x000000000000059f452a5f7340de6682a977387c17010ff6e6c3bd83ca8b1317"))
         (210000, uint256("0x000000000000048b95347e83192f69cf0366076336c639f9b7228e9ba171342e"))
         (216116, uint256("0x00000000000001b4f4b433e81ee46494af945cf96014816a4e2370f11b23df4e"))
         (225430, uint256("0x00000000000001c108384350f74090433e7fcf79a606b8e797f065b130575932"))
+		*/
         ;
     static const CCheckpointData data = {
         &mapCheckpoints,
-        1363044259, // * UNIX timestamp of last checkpoint block
-        14264869,   // * total number of transactions between genesis and last checkpoint
+        1307882499, // * UNIX timestamp of last checkpoint block
+        19199,      // * total number of transactions between genesis and last checkpoint
                     //   (the tx=... number in the SetBestChain debug.log lines)
-        60000.0     // * estimated number of transactions per day after checkpoint
+        600.000     // * estimated number of transactions per day after checkpoint
     };
 
     static MapCheckpoints mapCheckpointsTestnet = 
         boost::assign::map_list_of
-        ( 546, uint256("000000002a936ca763904c3c35fce2f3556c559c0214345d31b1bcebf76acb70"))
+        ( 546, uint256("00000000239a2153f15072e6fd14faece6a807a4f2aff40a3b0b1c7ede93794f"))
         ;
     static const CCheckpointData dataTestnet = {
         &mapCheckpointsTestnet,
-        1338180505,
-        16341,
-        300
+        1306893650,
+        547,
+        3
     };
 
     const CCheckpointData &Checkpoints() {
